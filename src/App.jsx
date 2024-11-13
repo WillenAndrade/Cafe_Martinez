a3fde3944d0b323665d32fc77a2e34382c8237f3
Willen Andrade <willen_jhefherson@hotmail.com>
2024-11-13 14:02:39 -0300

    Ajustando banner em sequencia

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Carrousel from './components/Carrousel'
 import { Link } from 'react-router-dom'
 
 function App() {
+
+    const [messageIndex, setMessageIndex] = useState(0);
+
+    useEffect(() => {
+      const intervalId = setInterval(nextPromotionMessage, 5000);  
+      return () => clearInterval(intervalId);
+  }, []);
+
+  const nextPromotionMessage = () => {
+    setMessageIndex((prevIndex) => (prevIndex + 1) % promotionMessage.length);
+    console.log(`Este é o Index do promotionMessage: ${messageIndex}`)
+  }
+
+  const promotionMessage = [
+    { message: "Tem promoção a partir da segunda unidade!",
+      style: "description-p-one"
+    },
+    { message: "Entregamos na porta de sua casa!",
+      style: "description-p-two"
+    },
+    { message: "Peça seu café já!",
+      style: "description-p-two"
+    }
+  ]
 
   return (
     <>
@@ -23,16 +47,19 @@ function App() {
           <h3>Produzido com grãos selecionados, nosso café é ideal para quem busca qualidade em um sabor suave  e autenticidade  incomparável!</h3>
       </div>
       <div className="line"></div>
+
+      <div className="buy">
+        {/* <button>Garanta já o seu!</button>*/}
+      </div>   
+      <div className="line"></div>
       <Carrousel />
       <div className="line"></div>
-      <div className="description">
-          <h3>O preço por embalagem cai a partir de 2 unidades!</h3>
+      <div className="description-two">
+          <p className={promotionMessage[messageIndex].style}>{promotionMessage[messageIndex].message}</p>
       </div>
-      <div className="test"></div>
-      <div className="buy">
-        <button>Garanta já o seu!</button>
-      </div>
-                
+      <div className="line"></div>
+      
+        
     </>
   )
 }
